213dcee339db080fc65cd80d3f6053b9ee59e9a6
shilpamis428@gmail.com <shilpamis428@gmail.com>
2024-04-29 23:05:03 +0530

    some

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -5,10 +5,10 @@ const Header = () => {
   return (
     <div className='header'>
         <div className="header-contents">
-            <h2>See Beauty of Banaras</h2>
+            <h2>See Beauty of Ajay</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, expedita aliquam?</p>
             <button>View Menu</button> 
-        </div>
+        </div> 
     </div>
   )
 }
